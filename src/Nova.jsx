94c884269a3bcc1c8968f4017dfcf336f236ed3b
Felipe Guizar Diaz <felipegaiacharly@gmail.com>
2019-08-12 21:57:59 -0500

    Add basic nova component

---
+++ src/Nova.jsx
@@ -0,0 +1,49 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { v4 } from 'uuid';
+
+const { window, CustomEvent, document } = global;
+
+class Nova extends Component {
+  constructor(props) {
+    super(props);
+    this.uuid = v4();
+    this.placeholder = React.createRef();
+  }
+
+  componentDidMount() {
+    if (window !== 'undefined') {
+      const id = this.placeholder.current.getAttribute('data-hypernova-id');
+      const { name } = this.props;
+      const customEvent = new CustomEvent('NovaMounted', { detail: { id, name } });
+      document.dispatchEvent(customEvent);
+    }
+  }
+
+  render() {
+    const { name, data } = this.props;
+    return (
+      <>
+        <div
+          ref={this.placeholder}
+          data-hypernova-key={name}
+          data-hypernova-id={this.uuid}
+          dangerouslySetInnerHTML={{ __html: '' }} // eslint-disable-line react/no-danger
+        />
+        <script
+          type="application/json"
+          data-hypernova-key={name}
+          data-hypernova-id={this.uuid}
+          dangerouslySetInnerHTML={{ __html: `<!--${JSON.stringify(data)}-->` }} // eslint-disable-line react/no-danger
+        />
+      </>
+    );
+  }
+}
+
+Nova.propTypes = {
+  name: PropTypes.string.isRequired,
+  data: PropTypes.shape({}).isRequired,
+};
+
+export default Nova;
---
+++ src/index.js
@@ -0,0 +1,1 @@
+export { default as Nova } from './Nova'; // eslint-disable-line import/prefer-default-export
